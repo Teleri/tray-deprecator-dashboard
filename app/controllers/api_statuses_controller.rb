e567db0ded281fac7df672297b9995111a068742
alec wayman <aejw_edu@pm.me>
2021-03-20 21:44:51 +0000

    controller and db calls

--- app/controllers/api_statuses_controller.rb
+++ app/controllers/api_statuses_controller.rb
@@ -1,4 +1,13 @@
 class ApiStatusesController < ApplicationController
   def index
+    @deprecated_apis = ApiStatus.where("is_deprecated")
+    @sunset_apis = ApiStatus.where("has_sunset")
+    @apis = ApiStatus.where.not("has_sunset").and(ApiStatus.where.not("is_deprecated"))
   end
+
+  # def manual_api_check
+  #   api = ApiStatus.first
+  #   CheckApiJob.perform_now(api)
+  #   redirect_to :api_statuses
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :api_statuses
+  post 'manual_api_check' => 'api_statuses_controller#manual_api_check', as: :api_check
   require "sidekiq/web"
   # authenticate :user, ->(user) { user.admin? } do
   #   mount Sidekiq::Web => '/sidekiq'
